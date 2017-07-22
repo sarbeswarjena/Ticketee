3337b402d17787e2f1df18435fdc6c704edfccc3
sarbeswarjena <sarbeswarjena@yahoo.co.in>
2017-07-22 12:01:24 +0530

    Implement ticket display

---
+++ spec/factories/ticket_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+	factory :ticket do
+		name "Example ticket"
+		description "An example ticket noting more"
+	end
+	
+end
---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+RSpec.feature "Users can view tickets" do
+	before do
+		sublime = FactoryGirl.create(:project, name: "Sublime Text 3")
+		FactoryGirl.create(:ticket, project:sublime, name:"Make it shiny!",
+				description:"Gradients! Starbursts! oh my!")
+		ie = FactoryGirl.create(:project, name:"Internet  Explorer")
+		FactoryGirl.create(:ticket, project:ie, name:"Standard compliance", description:"Isn't a joke.")
+		visit "/"
+	end
+	scenario "for a given project" do
+		click_link "Sublime Text 3"
+		expect(page).to have_content "Make it shiny!"
+		expect(page).to_not have_content "Standard compliance"
+		click_link "Make it shiny!"
+		within("#ticket h2") do
+			expect(page).to have_content "Make it shiny!"
+		end
+
+		expect(page).to have_content "Gradients! Starbursts! oh my!"
+	end
+end
